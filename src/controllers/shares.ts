50db119f597204e7a1a04a07ebacb2c7f375c8ef
Charles Mutagorama <cmutagorama@gmail.com>
2022-05-04 14:11:55 +0200

    changed logic

--- src/controllers/shares.ts
+++ src/controllers/shares.ts
@@ -8,66 +8,131 @@ const shareRepository = AppDataSource.getRepository(Share);
 
 const getShares = async (type, order) => {
   return shareRepository.find({
-    where: { type, quantity: MoreThan(0) },
-    select: ["quantity", "price"],
-    order: { price: order },
+    where: { type, qty: MoreThan(0) },
+    select: ["qty", "prc"],
+    order: { prc: order },
   });
 };
 
 const sell = async (req: Request, res: Response, next: NextFunction) => {
-  const { quantity, price } = req.body;
+  const { qty, prc } = req.body;
   const share = await shareRepository.findOne({
-    where: { type: "buy", price },
+    where: { type: "buy", prc },
   });
 
   if (share) {
     const highestShare = await shareRepository.findOne({
       where: { type: "buy" },
-      order: { price: "DESC" },
+      order: { prc: "DESC" },
     });
-    let remainder = 0;
-    if (highestShare.quantity < quantity) {
-      remainder = quantity - highestShare.quantity;
-      highestShare.quantity = 0;
-      share.quantity -= remainder;
+    if (qty > share.qty) {
+      const sellShare = await shareRepository.findOne({
+        where: { type: "sell", prc },
+      });
+
+      const remainder = qty - share.qty;
+      share.qty = 0;
+      await shareRepository.save(share);
+      if (sellShare) {
+        sellShare.qty += remainder;
+        await shareRepository.save(sellShare);
+      } else {
+        const newShare = shareRepository.create({
+          qty: remainder,
+          prc,
+          type: "sell",
+        });
+        await shareRepository.save(newShare);
+      }
+      return response({ res, code: 201, data: { share } });
     } else {
-      highestShare.quantity -= quantity;
+      let remainder = 0;
+      if (highestShare.qty < qty) {
+        remainder = qty - highestShare.qty;
+        highestShare.qty = 0;
+        share.qty -= remainder;
+      } else {
+        highestShare.qty -= qty;
+      }
+      await shareRepository.save(share);
+      const savedShare = await shareRepository.save(highestShare);
+      return response({ res, code: 200, data: { share: savedShare } });
     }
-    await shareRepository.save(share);
-    const savedShare = await shareRepository.save(highestShare);
-    return response({ res, code: 200, data: { share: savedShare } });
   }
 
-  const newShare = shareRepository.create({ quantity, price, type: "sell" });
+  const sellShare = await shareRepository.findOne({
+    where: { type: "sell", prc },
+  });
+
+  if (sellShare) {
+    sellShare.qty = sellShare.qty + parseInt(qty);
+    await shareRepository.save(sellShare);
+    return response({ res, code: 200, data: { share: sellShare } });
+  }
+
+  const newShare = shareRepository.create({ qty, prc, type: "sell" });
   const savedShare = await shareRepository.save(newShare);
   return response({ res, code: 201, data: { share: savedShare } });
 };
 
 const buy = async (req: Request, res: Response, next: NextFunction) => {
-  const { quantity, price } = req.body;
+  const { qty, prc } = req.body;
   const share = await shareRepository.findOne({
-    where: { type: "sell", price },
+    where: { type: "sell", prc },
   });
 
   if (share) {
     const lowestShare = await shareRepository.findOne({
       where: { type: "sell" },
-      order: { price: "ASC" },
+      order: { prc: "ASC" },
     });
-    let remainder = 0;
-    if (lowestShare.quantity < quantity) {
-      remainder = quantity - lowestShare.quantity;
-      lowestShare.quantity = 0;
-      share.quantity -= remainder;
+    if (qty > share.qty) {
+      const buyShare = await shareRepository.findOne({
+        where: { type: "buy", prc },
+      });
+
+      const remainder = qty - share.qty;
+      share.qty = 0;
+      await shareRepository.save(share);
+      if (buyShare) {
+        buyShare.qty += remainder;
+        await shareRepository.save(buyShare);
+      } else {
+        const newShare = shareRepository.create({
+          qty: remainder,
+          prc,
+          type: "buy",
+        });
+        await shareRepository.save(newShare);
+      }
+      return response({ res, code: 201, data: { share } });
     } else {
-      lowestShare.quantity -= quantity;
+      let remainder = 0;
+      if (lowestShare.qty < qty) {
+        remainder = qty - lowestShare.qty;
+        lowestShare.qty = 0;
+        share.qty -= remainder;
+      } else {
+        lowestShare.qty -= qty;
+      }
+
+      await shareRepository.save(share);
+      const savedShare = await shareRepository.save(lowestShare);
+      return response({ res, code: 200, data: { share: savedShare } });
     }
-    await shareRepository.save(share);
-    const savedShare = await shareRepository.save(lowestShare);
-    return response({ res, code: 200, data: { share: savedShare } });
   }
 
-  const newShare = shareRepository.create({ quantity, price, type: "buy" });
+  const buyShare = await shareRepository.findOne({
+    where: { type: "buy", prc },
+  });
+
+  if (buyShare) {
+    buyShare.qty = buyShare.qty + parseInt(qty);
+    await shareRepository.save(buyShare);
+    return response({ res, code: 200, data: { share: buyShare } });
+  }
+
+  const newShare = shareRepository.create({ qty, prc, type: "buy" });
   const savedShare = await shareRepository.save(newShare);
   return response({ res, code: 201, data: { share: savedShare } });
 };
